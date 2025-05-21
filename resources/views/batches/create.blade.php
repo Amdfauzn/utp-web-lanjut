0ba499a807c4594fdbc6d1662bb96ae18f706f87
Amdfauzn <143427605+Amdfauzn@users.noreply.github.com>
2025-05-21 17:11:27 +0700

    part9

--- resources/views/batches/create.blade.php
+++ resources/views/batches/create.blade.php
@@ -1,23 +1,78 @@
-@extends('layout')
-@section('content')
- 
-<div class="card">
-  <div class="card-header">Batches</div>
-  <div class="card-body">
-      
-  <form action="{{ url('batches') }}" method="post">
-    {!! csrf_field() !!}
-    <label>Batch Name</label></br>
-    <input type="text" name="name" id="name" class="form-control"></br>
-    <label>Course Name</label></br>
-    <input type="text" name="course_id" id="course_id" class="form-control"></br>
-    <label>Start Date</label></br>
-    <input type="text" name="start_date" id="start_date" class="form-control"></br>
-    <input type="submit" value="Save" class="btn btn-success"></br>
-</form>
+<?php
 
-   
-  </div>
-</div>
- 
-@stop
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Response;
+use App\Models\Batch;
+use App\Models\Course;
+use Illuminate\View\View;
+
+
+class BatchController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        $batches = Batch::all();
+        return view ('batches.index')->with('batches', $batches);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): View
+    {
+        $courses = Course::pluck('name','id');
+        return view('batches.create', compact('courses'));
+
+        // return view('batches.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $input = $request->all();
+        Batch::create($input);
+        return redirect('batches')->with('flash_message', 'Batch Addedd!');
+    }
+
+    
+    public function show(string $id): View
+    {
+        $batches = Batch::find($id);
+        return view('batches.show')->with('batches', $batches);  
+    }
+
+    
+    public function edit(string $id): View
+    {
+        $batches = Batch::find($id);
+        return view('batches.edit')->with('batches', $batches);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id): RedirectResponse
+    {
+        $batches = Batch::find($id);
+        $input = $request->all();
+        $batches->update($input);
+        return redirect('batches')->with('flash_message', 'Batch Updated!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id): RedirectResponse
+    {
+        Batch::destroy($id);
+        return redirect('batches')->with('flash_message', 'Batch deleted!');
+    }
+}
